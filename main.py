faf1f9d2ca9d790feb7dce6c6d517379ae760e4c
BenGorrr <ggbengaben@gmail.com>
2021-01-08 23:44:18 +0800

    Added required files for heroku

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import create_app
 from config import Config
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv('.env') #the path to your .env file (or any other file of environment variables you want to load)
+#load_dotenv('.env') #the path to your .env file (or any other file of environment variables you want to load)
 
 app = create_app(Config)
