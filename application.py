73e0102a01e6229ba320c4a53bc5155f41851e3d
BenGorrr <ggbengaben@gmail.com>
2021-01-08 18:55:27 +0800

    Commit test

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:76541@localhost:5432/web"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+test
 db.init_app(app)
 with app.app_context():
     db.create_all()
